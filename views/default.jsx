024173524895250298a18b602d08e3289993f807
wguadalupe <w.guadalupe78@gmail.com>
2024-01-21 14:14:39 -0500

    adding home page view

--- views/default.jsx
+++ views/default.jsx
@@ -1,15 +1,16 @@
-const React = require('react')
+const React = require('react');
 
-
-function Def (html) {
-    <html>
-        <head>
-            <title>Title</title>
-        </head>
-        <body>
-            {html.children}
-        </body>
-    </html>
+function Def(html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    );
 }
 
-module.exports = Def
+module.exports = Def;
