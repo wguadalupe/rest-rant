6959e911d2952e4eeca9b75d1c9c4240bfda418c
wguadalupe <w.guadalupe78@gmail.com>
2024-01-25 20:33:15 -0500

    Updated the form

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,31 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
+// Route for '/places/new'
+router.get('/new', (req, res) => {
+    res.render('places/new');
+});
+
+// Route for '/places/:id' (specific place details)
+router.get('/:id', (req, res) => {
+    // Placeholder for fetching a specific place by its id
+    const id = req.params.id;
+    // Placeholder object
+    let place = {
+        id: id,
+        name: 'Sample Place',
+        city: 'Sample City',
+        state: 'Sample State',
+        cuisines: 'Sample Cuisine',
+        pic: 'http://placekitten.com/250/250'
+    };
+
+    // Render the specific place details page
+    res.render('places/show', { place }); // Corrected here
+});
+
+
+// Route for '/' (list of places)
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
@@ -14,9 +39,9 @@ router.get('/', (req, res) => {
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
         pic: 'http://placekitten.com/250/250'
-    }]
+    }];
     
-    res.render('places/index', { places })
-})
+    res.render('places/index', { places });
+});
 
-module.exports = router
+module.exports = router;
